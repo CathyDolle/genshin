d5e0a926cbbd13a14c0b24f5d33b142332adfa13
cchampou <clement@champouillon.com>
2020-11-05 18:42:52 +0100

    Victory !

--- src/components/FadeOutIn.js
+++ src/components/FadeOutIn.js
@@ -21,20 +21,22 @@ function FadeOutIn({ src, time, alt, className, ...props }) {
   const [currentSrc, setCurrentSrc] = useState(src)
   const [animation, setAnimation] = useState(false);
   const prevSrc = usePrevious(src)
+  const timer = useRef(null)
+  const target = useRef(null)
 
   useEffect(() => {
-    let timer;
-    if (prevSrc) {
+    target.current = src;
+    if (prevSrc && !timer.current) {
       setAnimation(true);
-      timer = setTimeout(() => {
-        setCurrentSrc(src);
+      timer.current = setTimeout(() => {
+        setCurrentSrc(target.current);
         setAnimation(false);
-      }, (time * 1000) / 2)
+        timer.current = null;
+      }, (time * 1000) / 2);
     }
 
     return () => {
       clearTimeout(timer);
-      setAnimation(false);
     }
   }, [src, time])
 
@@ -45,5 +47,7 @@ function FadeOutIn({ src, time, alt, className, ...props }) {
             {...props}
           />
 }
+
+
 
 export default FadeOutIn
