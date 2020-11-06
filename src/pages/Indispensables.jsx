171893ca5817da2b33a76db2d5d94fb91474f193
CathyDolle <cathy.dolle@live.fr>
2020-11-06 01:05:15 +0100

    added char left :sparkles:

--- src/pages/Indispensables.jsx
+++ src/pages/Indispensables.jsx
@@ -19,14 +19,16 @@ function Indispensables() {
       <Visual src={CurrentElement.leftChar} position="left" height="95%" />
       <Content>
         {article.map(({
-          title, text, src, linkName, link,
+          title, text, src, hrefName, href, to, toName,
         }) => (
           <TextContent
             title={title}
-            hrefName={linkName}
-            href={link}
+            hrefName={hrefName}
+            href={href}
             src={src}
             text={text}
+            to={to}
+            toName={toName}
           />
         ))}
       </Content>
