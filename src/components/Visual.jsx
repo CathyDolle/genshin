30127e762899caa47cac19532664f13aa350cd18
Clément Champouillon <clement@champouillon.com>
2020-11-06 05:11:48 +0100

    fix(animation): positionning (#13)

--- src/components/Visual.jsx
+++ src/components/Visual.jsx
@@ -3,23 +3,31 @@ import PropTypes from 'prop-types';
 import FadeOutIn from '../utils/FadeOutIn';
 
 const Visual = ({
-  src, position,
+  src, position, height,
 }) => (
   <section className="visual">
     <FadeOutIn
       src={src}
       alt="visual"
-      enterAnimationName={position === 'left' ? 'charAnimationLeft' : 'charAnimationRight'}
+      className={position}
+      // TODO: Refactor this ternary condition. This should not exist.
+      enterAnimationName={position === 'left' || position === 'xiao' ? 'charAnimationLeft' : 'charAnimationRight'}
       enterAnimationDuration={500}
       leaveAnimationName="fadeCharAnimation"
       leaveAnimationDuration={1000}
+      height={height}
     />
   </section>
 );
 
+Visual.defaultProps = {
+  height: null,
+};
+
 Visual.propTypes = {
   src: PropTypes.string.isRequired,
   position: PropTypes.string.isRequired,
+  height: PropTypes.string,
 };
 
 export default Visual;
