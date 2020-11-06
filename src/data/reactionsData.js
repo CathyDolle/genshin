03abc07ed4241f7def16f7911d574afad15cd49b
Pilna <pilna@outlook.fr>
2020-11-07 00:52:02 +0100

    fix reaction :poop:

--- src/data/reactionsData.js
+++ src/data/reactionsData.js
@@ -2,46 +2,55 @@ const reactions = [
   {
     name: 'Swirl',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed convallis porttitor sapien',
+    search: ['anemo'],
     types: ['pyro', 'hydro', 'cryo', 'electro', 'anemo'],
   },
   {
     name: 'Crystallize',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed convallis porttitor sapien',
+    search: ['geo'],
     types: ['pyro', 'hydro', 'cryo', 'electro', 'geo'],
   },
   {
     name: 'Electrocution',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed convallis porttitor sapien',
+    search: ['hydro', 'electro'],
     types: ['hydro', 'electro'],
   },
   {
     name: 'Superconduct',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed convallis porttitor sapien',
+    search: ['cryo', 'electro'],
     types: ['cryo', 'electro'],
   },
   {
     name: 'Overload',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed convallis porttitor sapien',
+    search: ['pyro', 'electro'],
     types: ['pyro', 'electro'],
   },
   {
     name: 'Burning',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed convallis porttitor sapien',
+    search: ['pyro', 'dendro'],
     types: ['pyro', 'dendro'],
   },
   {
     name: 'Vaporize',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed convallis porttitor sapien',
+    search: ['pyro', 'hydro'],
     types: ['pyro', 'hydro'],
   },
   {
     name: 'Freeze',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed convallis porttitor sapien',
+    search: ['cryo', 'hydro'],
     types: ['cryo', 'hydro'],
   },
   {
     name: 'Melt',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed convallis porttitor sapien',
+    search: ['cryo', 'pyro'],
     types: ['cryo', 'pyro'],
   },
 ];
