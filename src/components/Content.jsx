72429701a9aca8147f8461010f26223a00ee1965
CathyDolle <cathy.dolle@live.fr>
2020-11-07 03:46:45 +0100

    Edited scroll bar in text content

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,11 +1,15 @@
+import CustomScroll from 'react-custom-scroll';
+import 'react-custom-scroll/dist/customScroll.css';
 import PropTypes from 'prop-types';
 import React from 'react';
 import './Content.scss';
 
 const Content = ({ children }) => (
-  <section className="content">
-    {children}
-  </section>
+  <CustomScroll heightRelativeToParent="55%">
+    <section className="content">
+      {children}
+    </section>
+  </CustomScroll>
 );
 
 Content.defaultProps = {
--- src/data/reactionsData.js
+++ src/data/reactionsData.js
@@ -1,55 +1,64 @@
 const reactions = [
   {
     name: 'Swirl',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed convallis porttitor sapien',
+    description: `Absorbe le premier élément entrant en contact avec celle-ci 
+                  et déclenche des dégâts élémentaires propres aux éléments 
+                  utilisés qui ont générer plusieurs autres réactions élémentaires.`,
     search: ['anemo'],
     types: ['pyro', 'hydro', 'cryo', 'electro', 'anemo'],
   },
   {
     name: 'Crystallize',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed convallis porttitor sapien',
+    description: `Génère des fragments élémentaires qui fournissent des boucliers des 
+                  éléments correspondants, qui vont eux, réduire les dommages 
+                  élémentaires à venir.`,
     search: ['geo'],
     types: ['pyro', 'hydro', 'cryo', 'electro', 'geo'],
   },
   {
     name: 'Electrocution',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed convallis porttitor sapien',
+    description: 'Déclenche des dégâts electro sur la durée pendant un certain temps',
     search: ['hydro', 'electro'],
     types: ['hydro', 'electro'],
   },
   {
     name: 'Superconduct',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed convallis porttitor sapien',
+    description: `Déclenche des dégâts CRYO en 
+                  zone et réduit La Défense de la 
+                  cible`,
     search: ['cryo', 'electro'],
     types: ['cryo', 'electro'],
   },
   {
     name: 'Overload',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed convallis porttitor sapien',
+    description: 'Crée une explosion qui réalise des dégâts Pyro en zone et permet de détruire les boucliers de types GEO',
     search: ['pyro', 'electro'],
     types: ['pyro', 'electro'],
   },
   {
     name: 'Burning',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed convallis porttitor sapien',
+    description: 'Déclenche des dégâts Pyro sur la durée.',
     search: ['pyro', 'dendro'],
     types: ['pyro', 'dendro'],
   },
   {
     name: 'Vaporize',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed convallis porttitor sapien',
+    description: 'Déclenche des dégâts élémentaires additionnels, l’effet se dissipe une fois déclenché',
     search: ['pyro', 'hydro'],
     types: ['pyro', 'hydro'],
   },
   {
     name: 'Freeze',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed convallis porttitor sapien',
+    description: `Le gel empêche les cibles de 
+                  bouger, brisé est déclenché si des 
+                  dégâts physiques sont déclenchés 
+                  sur des cibles gelées.`,
     search: ['cryo', 'hydro'],
     types: ['cryo', 'hydro'],
   },
   {
     name: 'Melt',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed convallis porttitor sapien',
+    description: 'Déclenche une fusion venant des éléments Cryo ou Pryo, augmentant les dommages effectués ',
     search: ['cryo', 'pyro'],
     types: ['cryo', 'pyro'],
   },
