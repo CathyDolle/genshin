3aa06888ba305deaef6b449e169171c90f76ef12
Pilna <pilna@outlook.fr>
2020-11-07 05:25:24 +0100

    fix link fetch news

--- src/redux/thunks/getNews.js
+++ src/redux/thunks/getNews.js
@@ -3,7 +3,7 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 const getNews = createAsyncThunk(
   'news/get',
   async () => {
-    const response = await fetch('https://genshin.cchampou.me/news');
+    const response = await fetch('https://genshin.cchampou.me/news-items');
     return response.json();
   },
 );
